ed0623156742729bb36f4228ae937f65c8227047
Mukesh M <mukesh192k@gmail.com>
2020-08-17 18:40:39 +0530

    update

--- linkerlist.cpp
+++ linkerlist.cpp
@@ -44,22 +44,39 @@ void merger(Node** po,Node *h2)
     while ((h1->next)!=NULL)h1=h1->next;
     h1->next=h2;
 }
+Node* sortedMerge(Node* head1, Node* head2)  /*important*/
+{  
+    
+    if(head1 == NULL)
+        return head2;
+    if(head2 == NULL)
+        return head1;
+
+    if(head1->data < head2->data) {
+    head1->next = sortedMerge(head1->next,head2);
+    return head1;
+    } 
+    else {
+    head2->next = sortedMerge(head1,head2->next);
+    return head2;
+    }
+}  
 main()
 {
-    Node *head=NULL;
-    append(&head,75);
+    Node *head=NULL;  //firstlist
+    append(&head,10);
     push(&head,15);
     push(&head,75);
     insertafter(head,65);
     append(&head,45);
-    print (head);
-    Node *hea=NULL;
+    //print (head);
+    Node *hea=NULL;  //secondlist
     append(&hea,75);
     push(&hea,15);
     push(&hea,75);
     insertafter(hea,65);
     append(&hea,45);
-    merger(&head,hea);
+    head=sortedMerge(head,hea);
     print(head);
 
 }
---
+++ sorts.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#define A 6
+using namespace std;
+
+void merger(int a[], int l, int m, int r)
+{
+    int n1 = m - l + 1, n2 = r - m;
+    int L[n1], R[n2];
+    int i, j, k;
+    for (i = 0; i < n1; i++)
+        L[i] = a[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = a[1 + m + j];
+    i = 0, j = 0, k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            a[k] = L[i];
+            i++;
+        }
+        else
+        {
+            a[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1)
+    {
+        a[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        a[k] = R[j];
+        j++;
+        k++;
+    }
+}
+void mergesort(int a[], int l, int r)
+{
+    if (l < r)
+    {
+        int i = 0;
+        int m = (l + (r - 1)) / 2;
+        mergesort(a, l, m);
+        mergesort(a, m + 1, r);
+        merger(a, l, m, r);
+        // cout<<i++;
+    }
+}
+void swap(int *a, int *b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(int *a, int l, int h)
+{
+    int pi = a[h], i = l, j = 0;
+    for (j = l; j <= h - 1; j++)
+        if (a[j] < pi) swap(a + i++, a + j);
+    swap(a + h, a + i);
+    return i ;
+}
+void quicksort(int *a, int l, int h)
+{
+    if (l < h)
+    {
+        int pi = partition(a, l, h);
+        quicksort(a, l, pi - 1);
+        quicksort(a, pi + 1, h);
+    }
+}
+main()
+{
+    int a[A] = {2, 65, 6, 6, 4, 45};
+    quicksort(a, 0, A - 1);
+    int i;
+    for (i = 0; i < A; i++)
+        cout << a[i] << endl;
+}
